401f575289cc55ad053e33f2402a6875d37554f6
JeasunLok <1913587141@qq.com>
2023-07-12 15:56:45 +0800

    add: tif read and write

--- utils/tif_processing.py
+++ utils/tif_processing.py
@@ -1,5 +1,34 @@
 import numpy as np
 from osgeo import gdal
+# 读取tif
+def read_tif(path):
+    dataset = gdal.Open(path)
+    cols = dataset.RasterXSize # 图像长度
+    rows = (dataset.RasterYSize) # 图像宽度
+    im_proj = (dataset.GetProjection()) # 读取投影
+    im_Geotrans = (dataset.GetGeoTransform()) # 读取仿射变换
+    im_data = dataset.ReadAsArray(0, 0, cols, rows) # 转为numpy格式
+    del dataset
+    return im_data, im_Geotrans, im_proj, cols, rows
+
+# 写出tif
+def write_tif(newpath, im_data, im_geotrans, im_proj, datatype):
+    # datatype常用gdal.GDT_UInt16 gdal.GDT_Int16 gdal.GDT_Float32
+    if len(im_data.shape)==3:
+        im_bands, im_height, im_width = im_data.shape
+    else:
+        im_bands, (im_height, im_width) = 1, im_data.shape
+    driver = gdal.GetDriverByName('GTiff')
+    new_dataset = driver.Create(newpath, im_width, im_height, im_bands, datatype)
+    new_dataset.SetGeoTransform(im_geotrans)
+    new_dataset.SetProjection(im_proj)
+
+    if im_bands == 1:
+        new_dataset.GetRasterBand(1).WriteArray(im_data.reshape(im_height, im_width))
+    else:
+        for i in range(im_bands):
+            new_dataset.GetRasterBand(i+1).WriteArray(im_data[i])
+    del new_dataset
 
 # 为影像写入坐标系的函数（创建新tif）
 # def geoCoordSys(read_path, img_transf, img_proj):
