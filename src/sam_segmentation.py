cf56a40f8d8db32e8883a902c2102ed8892b1c76
JeasunLok <1913587141@qq.com>
2023-07-12 15:56:35 +0800

    add: segmentation

---
+++ src/sam_segmentation.py
@@ -0,0 +1,20 @@
+import samgeo
+import torch
+from samgeo import SamGeo
+
+def sam_segmentation(image_path, segmentation_path, str_device):
+    checkpoint = r'asset\sam_vit_b_01ec64.pth'
+
+    if str_device == "GPU":
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    else:
+        device = 'cpu'
+    
+    sam = SamGeo(
+        checkpoint=checkpoint,
+        model_type='vit_b',
+        device=device,
+        sam_kwargs=None,
+    )
+
+    sam.generate(image_path, segmentation_path)
